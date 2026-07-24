ac71b60ef39746e5166e03ae4596232e407746a5
Aditya Phadke <adi.phadke9@gmail.com>
2026-07-24 17:05:13 +0530

    Freshedesk Integration

---
+++ force-app/main/default/lwc/freshdeskTicket/freshdeskTicket.js
@@ -0,0 +1,79 @@
+import { LightningElement } from 'lwc';
+import createTicket from '@salesforce/apex/FreshDeskTicketController.createTicket';
+
+export default class FreshdeskTicket extends LightningElement {
+    ticketInformation = {
+        "status":'2'
+    };
+    get statusoptions(){
+        return [
+            {label:'Open',value:'2'}
+        ]
+    }
+
+    get typeoptions(){
+        return [
+            {label:'Incident',value:'Incident'},
+            {label:'Problem',value:'Problem'}
+        ]
+    }
+
+    get sourceoptions(){
+        return [
+            {label:'Email',value:'1'},
+            {label:'Portal',value:'2'},
+            {label:'Phone',value:'3'},
+            {label:'Chat',value:'7'}
+        ]
+    }
+
+        get priorityoptions(){
+        return [
+            {label:'Low',value:'1'},
+            {label:'Medium',value:'2'},
+            {label:'High',value:'3'},
+            {label:'Urgent',value:'4'}
+        ]
+    }
+
+    handleClick(event){
+        event.preventDefault();
+         const allInputs = this.template.querySelectorAll(
+            'lightning-input, lightning-combobox, lightning-textarea, lightning-radio-group');
+
+        // 2. Reduce the collection to a single boolean
+        const allValid = [...allInputs].reduce((validSoFar, field) => {
+            // Trigger the UI error message display
+            field.reportValidity();
+            
+            // Check its current validity status
+            return validSoFar && field.checkValidity();
+        }, true);
+
+        // 3. Process form data if everything passes
+        if (allValid) {
+            console.log('All fields are valid. Ready to submit!');
+            console.log(JSON.stringify(this.ticketInformation));
+            createTicket({inputMap : this.ticketInformation})
+            .then(result=>{
+                 console.log('result',result);
+            })
+            .catch(error=>{
+                console.log('Error',error);
+            })
+            .finally(()=>{
+
+            })
+            // Proceed with Apex call or navigation
+        } else {
+            console.log('Validation failed. Review the UI errors.');
+        }
+    }
+
+    handleInputChange(event){
+        event.preventDefault();
+        let name = event.target.name;
+        let value = event.target.value;
+        this.ticketInformation[name] = value;
+    }
+}
